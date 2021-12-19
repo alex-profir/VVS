5e688171dfecd662cd1f31be80cd186c695672f5
Alex Profir <alex_profir@yahoo.com>
2021-12-19 18:24:56 +0200

    updated webserver

--- src/server.ts
+++ src/server.ts
@@ -16,20 +16,21 @@ app.get("/maintanance", (req, res) => {
     maintananceMode = !maintananceMode;
     res.send(`Maintanance changed to ${maintananceMode}`);
 })
+const buildFile = "build"; // this should be used to indicate the file locations.
 app.get("/welcome", (req, res) => {
-    const files = fs.readdirSync("build", { encoding: "utf8" })
+    const files = fs.readdirSync(buildFile, { encoding: "utf8" })
     console.log({ files });
     res.render("Welcome", {
         data: files,
-        relPath: "/build/",
+        relPath: "/",
     });
-})
-app.use("/build", express.static("build"));
+});
+app.use(express.static(buildFile));
 app.use(express.static("public"));
 app.get("*", (req, res, next) => {
     try {
-
-        const files = fs.readdirSync(req.url.substr(1), { encoding: "utf8" })
+        const fileRequest = `${buildFile}/${req.url.substr(1)}`;
+        const files = fs.readdirSync(fileRequest, { encoding: "utf8" });
         res.render("Welcome", {
             data: files,
             relPath: req.url,
